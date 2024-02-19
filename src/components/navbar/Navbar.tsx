02ef78f0b06f61c59da0a012526386e3b4f21da6
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2024-02-19 21:01:04 +0100

    :rocket: :tada: main : create the Context  :rocket: :tada:

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,24 +1,27 @@
-import React from 'react'
-import "./Style.css"
-import Logo from "../navbar/logo.png"
+import React from "react";
+import "./Style.css";
+import Logo from "../navbar/logo.png";
+import { Link } from "react-router-dom";
 
 export const Navbar = () => {
   return (
-    <div className='nav'>
-        <div className='logo'>
-            <img src={Logo} alt="poto" />
-        </div>
-        <div className='ancres'>
-            <a href="">Home</a>
-            <a href="">About</a>
-        </div>
-        <div className='inp'>
-            <input 
-                type="text"
-                placeholder='Choisis ton plat ...'
-                className="input" 
-            />
-        </div>
+    <div className="nav">
+      <div className="logo">
+      <Link to="/">
+        <img src={Logo} alt="poto" />
+      </Link>
+      </div>
+      <div className="ancres">
+        <Link to="/">Home</Link>
+        <a href="">About</a>
+      </div>
+      <div className="inp">
+        <input
+          type="text"
+          placeholder="Choisis ton plat ..."
+          className="input"
+        />
+      </div>
     </div>
-  )
-}
+  );
+};
